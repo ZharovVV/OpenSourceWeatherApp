0bd1ba0ebeac125cc1fab9b5eb86ee12f4331889
Vitaly Zharov <zharov.vitalik@gmail.com>
2021-10-18 23:13:39 +0500

    Рефакторинг WeatherTodayFragment.

---
+++ app/src/main/java/com/github/zharovvv/open/source/weather/app/ui/view/RequestLocationPermissionFragment.kt
@@ -0,0 +1,109 @@
+package com.github.zharovvv.open.source.weather.app.ui.view
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.github.zharovvv.open.source.weather.app.R
+import com.github.zharovvv.open.source.weather.app.location.LocationPermissionExplanationDialogFragment
+import com.github.zharovvv.open.source.weather.app.location.LocationService
+import com.github.zharovvv.open.source.weather.app.location.LocationViewModel
+import com.github.zharovvv.open.source.weather.app.navigation.LOCATION_PERMISSION_EXPLANATION_DIALOG_RESULT_KEY
+import com.github.zharovvv.open.source.weather.app.navigation.setUpNavDialogResultCallback
+
+/**
+ * Базовый класс, для фрагмента, в котором необходимо запрашивать Location Permission.
+ * При предоставлении разрешения - запускает [LocationService].
+ * Наблюдать за изменением местоположения можно через [LocationViewModel.locationData].
+ * Для корректной работы при переопределении методов [onViewCreated] и [onDestroyView] в классах наследниках
+ * необходимо вызывать методы супер-класса.
+ */
+abstract class RequestLocationPermissionFragment : BaseFragment() {
+
+    private var requestLocationPermissionLauncher: ActivityResultLauncher<String>? = null
+    protected val locationViewModel: LocationViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController()
+        configureRequestLocationPermission(navController)
+        //Вызываем метод только один раз при первом запуске фрагмента
+        if (savedInstanceState == null && !isRestoredFromBackStack) {
+            requestLocation()
+        }
+    }
+
+    private fun configureRequestLocationPermission(navController: NavController) {
+        requestLocationPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                requestLocation()
+            } else {
+                onLocationPermissionIsNotGranted()
+            }
+        }
+        setUpNavDialogResultCallback(
+            navBackStackEntry = navController.getBackStackEntry(R.id.nav_weather_today),
+            resultKey = LOCATION_PERMISSION_EXPLANATION_DIALOG_RESULT_KEY
+        ) { result: LocationPermissionExplanationDialogFragment.DialogResult ->
+            when (result) {
+                LocationPermissionExplanationDialogFragment.DialogResult.CANCEL -> {
+                    navController.navigate(R.id.nav_location_permission_explanation)
+                }
+
+                LocationPermissionExplanationDialogFragment.DialogResult.SHOULD_REQUEST_LOCATION_PERMISSION -> {
+                    requestLocationPermissionLauncher?.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+                }
+            }
+        }
+    }
+
+    /**
+     * Вызов данного метода запускает работу [LocationService], таким образом в [LocationViewModel.locationData]
+     * начинают поступать актуальные данные о местоположении.
+     */
+    private fun requestLocation() {
+        if (checkLocationPermission()) {
+            locationViewModel.requestLocation()
+        }
+    }
+
+    /**
+     * TODO Возможно метод не нужен.
+     */
+    abstract fun onLocationPermissionIsNotGranted()
+
+    private fun checkLocationPermission(): Boolean {
+        val locationPermission = Manifest.permission.ACCESS_COARSE_LOCATION
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                locationPermission
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                return true
+            }
+
+            requireActivity().shouldShowRequestPermissionRationale(locationPermission) -> {
+                findNavController().navigate(R.id.nav_location_permission_explanation)
+            }
+
+            else -> {
+                //requestLocationPermissionLauncher?.launch(locationPermission)
+                findNavController().navigate(R.id.nav_location_permission_explanation)
+            }
+        }
+        return false
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        requestLocationPermissionLauncher = null
+    }
+}
--- app/src/main/java/com/github/zharovvv/open/source/weather/app/ui/weather/today/WeatherTodayFragment.kt
+++ app/src/main/java/com/github/zharovvv/open/source/weather/app/ui/weather/today/WeatherTodayFragment.kt
@@ -1,45 +1,30 @@
 package com.github.zharovvv.open.source.weather.app.ui.weather.today
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
-import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.navigation.NavController
+import androidx.lifecycle.LiveData
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.zharovvv.open.source.weather.app.R
 import com.github.zharovvv.open.source.weather.app.databinding.FragmentWeatherTodayBinding
-import com.github.zharovvv.open.source.weather.app.location.LocationPermissionExplanationDialogFragment.DialogResult
-import com.github.zharovvv.open.source.weather.app.location.LocationPermissionExplanationDialogFragment.DialogResult.CANCEL
-import com.github.zharovvv.open.source.weather.app.location.LocationPermissionExplanationDialogFragment.DialogResult.SHOULD_REQUEST_LOCATION_PERMISSION
-import com.github.zharovvv.open.source.weather.app.location.LocationViewModel
 import com.github.zharovvv.open.source.weather.app.model.DataState
+import com.github.zharovvv.open.source.weather.app.model.HourlyWeatherModel
 import com.github.zharovvv.open.source.weather.app.model.LocationModel
 import com.github.zharovvv.open.source.weather.app.model.WeatherTodayModel
-import com.github.zharovvv.open.source.weather.app.navigation.LOCATION_PERMISSION_EXPLANATION_DIALOG_RESULT_KEY
-import com.github.zharovvv.open.source.weather.app.navigation.setUpNavDialogResultCallback
-import com.github.zharovvv.open.source.weather.app.ui.view.BaseFragment
+import com.github.zharovvv.open.source.weather.app.ui.view.RequestLocationPermissionFragment
 import com.github.zharovvv.open.source.weather.app.util.getColorFromAttr
 
-class WeatherTodayFragment : BaseFragment() {
+class WeatherTodayFragment : RequestLocationPermissionFragment() {
 
     // This property is only valid between onCreateView and onDestroyView.
     private var _binding: FragmentWeatherTodayBinding? = null
     private val binding: FragmentWeatherTodayBinding get() = _binding!!
-    private var requestLocationPermissionLauncher: ActivityResultLauncher<String>? = null
-    private val locationViewModel: LocationViewModel by activityViewModels()
     private val weatherTodayViewModel: WeatherTodayViewModel by viewModels()
     private val hourlyWeatherViewModel: HourlyWeatherViewModel by viewModels()
 
@@ -49,87 +34,117 @@ class WeatherTodayFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentWeatherTodayBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val navController = findNavController()
-        val detailedWeatherParamsAdapter = DetailedWeatherParamsAdapter()
-        val hourlyWeatherAdapter = HourlyWeatherAdapter()
-        val hourlyWeatherItemOnScrollListener = HourlyWeatherItemOnScrollListener()
-        with(binding) {
-            toWeekWeatherButton.setOnClickListener {
-                navController.navigate(R.id.action_nav_weather_today_to_nav_week_weather)
-            }
-            detailedParamsRecyclerView.layoutManager =
-                GridLayoutManager(this@WeatherTodayFragment.context, 2)
-            detailedParamsRecyclerView.adapter = detailedWeatherParamsAdapter
-            hourlyWeatherRecyclerView.layoutManager =
-                LinearLayoutManager(
-                    this@WeatherTodayFragment.context,
-                    LinearLayoutManager.HORIZONTAL,
-                    false
+        with(binding.weatherTodaySwipeRefreshLayout) {
+            setColorSchemeColors(
+                requireContext().getColorFromAttr(
+                    R.attr.colorPrimary
                 )
-            hourlyWeatherRecyclerView.adapter = hourlyWeatherAdapter
-            hourlyWeatherRecyclerView.addOnScrollListener(hourlyWeatherItemOnScrollListener)
-
-            weatherTodaySwipeRefreshLayout.setOnRefreshListener {
-                weatherTodayViewModel.requestWeatherToday()
-                hourlyWeatherViewModel.requestHourlyWeather()
-                if (weatherTodaySwipeRefreshLayout.isRefreshing) {
-                    Handler(Looper.getMainLooper()).postDelayed({
-                        weatherTodaySwipeRefreshLayout.isRefreshing = false
-                    }, 1000L)
-                }
-            }
-            weatherTodaySwipeRefreshLayout.setColorSchemeColors(requireContext().getColorFromAttr(R.attr.colorPrimary))
-            weatherTodaySwipeRefreshLayout.setProgressBackgroundColorSchemeColor(
+            )
+            setProgressBackgroundColorSchemeColor(
                 requireContext().getColorFromAttr(
                     R.attr.colorPrimaryDark
                 )
             )
         }
+        return binding.root
+    }
 
-        hourlyWeatherItemOnScrollListener.timeIndicatorData.observe(viewLifecycleOwner) {
-            binding.timeIndicatorTextView.text = it.data.description
-            if (!it.isFirstValue) {
-                binding.timeIndicatorTextView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindHeader(
+            viewBinding = binding,
+            data = locationViewModel.locationData
+        )
+        bindWeatherTodayWidget(
+            viewBinding = binding,
+            data = weatherTodayViewModel.weatherTodayData
+        )
+        bindHourlyWeatherWidget(
+            viewBinding = binding,
+            data = hourlyWeatherViewModel.hourlyWeatherData
+        )
+        with(binding.weatherTodaySwipeRefreshLayout) {
+            setOnRefreshListener {
+                weatherTodayViewModel.requestWeatherToday()
+                hourlyWeatherViewModel.requestHourlyWeather()
             }
         }
+    }
 
-        configureRequestLocationPermission(navController)
-
-        locationViewModel.locationData.observe(viewLifecycleOwner) { locationModel: LocationModel ->
-            binding.cityNameTextView.text =
-                getString(R.string.delimiter_comma, locationModel.cityName)
-            binding.countryNameTextView.text = locationModel.countryName
+    private fun bindHeader(
+        viewBinding: FragmentWeatherTodayBinding,
+        data: LiveData<LocationModel>
+    ) {
+        data.observe(viewLifecycleOwner) { locationModel: LocationModel ->
+            with(viewBinding) {
+                cityNameTextView.text = getString(R.string.delimiter_comma, locationModel.cityName)
+                countryNameTextView.text = locationModel.countryName
+            }
         }
+    }
 
-        weatherTodayViewModel.weatherTodayData.observe(viewLifecycleOwner) { dataState: DataState<WeatherTodayModel> ->
-            when (dataState) {
-                is DataState.Success -> {
-                    val weatherTodayModel = dataState.data
-                    with(binding) {
+    private fun bindWeatherTodayWidget(
+        viewBinding: FragmentWeatherTodayBinding,
+        data: LiveData<DataState<WeatherTodayModel>>
+    ) {
+        val detailedWeatherParamsLayoutManager = GridLayoutManager(context, 2)
+        val detailedWeatherParamsAdapter = DetailedWeatherParamsAdapter()
+        with(viewBinding.detailedParamsRecyclerView) {
+            layoutManager = detailedWeatherParamsLayoutManager
+            adapter = detailedWeatherParamsAdapter
+        }
+        data.observe(viewLifecycleOwner) { dataState: DataState<WeatherTodayModel> ->
+            with(viewBinding) {
+                when (dataState) {
+                    is DataState.Success -> {
+                        val weatherTodayModel = dataState.data
                         weatherTodayProgressBar.isVisible = false
                         weatherTodayCardIconImageView.setImageResource(weatherTodayModel.iconId)
                         weatherDescriptionTextView.text = weatherTodayModel.description
                         todayDateTextView.text = weatherTodayModel.dateString
                         weatherTodayTemperatureTextView.text = weatherTodayModel.temperature
+                        detailedWeatherParamsAdapter.submitList(weatherTodayModel.detailedWeatherParams)
+                        weatherTodaySwipeRefreshLayout.isRefreshing = false
                     }
-                    detailedWeatherParamsAdapter.submitList(weatherTodayModel.detailedWeatherParams)
-                }
-                is DataState.Loading -> {
-                    binding.weatherTodayProgressBar.isVisible = true
-                }
-                is DataState.Error -> {
-                    binding.weatherTodayProgressBar.isVisible = false
-                    binding.weatherDescriptionTextView.text = dataState.message
+                    is DataState.Loading -> {
+                        weatherTodayProgressBar.isVisible = true
+                    }
+                    is DataState.Error -> {
+                        weatherTodayProgressBar.isVisible = false
+                        weatherDescriptionTextView.text = dataState.message
+                        weatherTodaySwipeRefreshLayout.isRefreshing = false
+                    }
                 }
             }
         }
+    }
 
-        hourlyWeatherViewModel.hourlyWeatherData.observe(viewLifecycleOwner) { dataState ->
+    private fun bindHourlyWeatherWidget(
+        viewBinding: FragmentWeatherTodayBinding,
+        data: LiveData<DataState<HourlyWeatherModel>>
+    ) {
+        val navController = findNavController()
+        val hourlyWeatherAdapter = HourlyWeatherAdapter()
+        val hourlyWeatherLayoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val hourlyWeatherItemOnScrollListener = HourlyWeatherItemOnScrollListener()
+        with(viewBinding) {
+            toWeekWeatherButton.setOnClickListener {
+                navController.navigate(R.id.action_nav_weather_today_to_nav_week_weather)
+            }
+            hourlyWeatherRecyclerView.layoutManager = hourlyWeatherLayoutManager
+            hourlyWeatherRecyclerView.adapter = hourlyWeatherAdapter
+            hourlyWeatherRecyclerView.addOnScrollListener(hourlyWeatherItemOnScrollListener)
+        }
+        hourlyWeatherItemOnScrollListener.timeIndicatorData.observe(viewLifecycleOwner) {
+            with(viewBinding) {
+                timeIndicatorTextView.text = it.data.description
+                if (!it.isFirstValue) {
+                    timeIndicatorTextView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+                }
+            }
+        }
+        data.observe(viewLifecycleOwner) { dataState ->
             when (dataState) {
                 is DataState.Success -> {
                     hourlyWeatherAdapter.submitList(dataState.data.items)
@@ -139,69 +154,14 @@ class WeatherTodayFragment : BaseFragment() {
                 }
             }
         }
-
-        if (savedInstanceState == null && !isRestoredFromBackStack) {
-            requestLocation()
-        }
     }
 
-    private fun configureRequestLocationPermission(navController: NavController) {
-        requestLocationPermissionLauncher = registerForActivityResult(
-            RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                requestLocation()
-            } else {
-                binding.cityNameTextView.text = "Доступ к локации не предоставлен"
-            }
-        }
-        setUpNavDialogResultCallback(
-            navBackStackEntry = navController.getBackStackEntry(R.id.nav_weather_today),
-            resultKey = LOCATION_PERMISSION_EXPLANATION_DIALOG_RESULT_KEY
-        ) { result: DialogResult ->
-            when (result) {
-                CANCEL -> {
-                    navController.navigate(R.id.nav_location_permission_explanation)
-                }
-
-                SHOULD_REQUEST_LOCATION_PERMISSION -> {
-                    requestLocationPermissionLauncher?.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
-                }
-            }
-        }
-    }
-
-    private fun requestLocation() {
-        if (checkLocationPermission()) {
-            locationViewModel.requestLocation()
-        }
-    }
-
-    private fun checkLocationPermission(): Boolean {
-        val locationPermission = Manifest.permission.ACCESS_COARSE_LOCATION
-        when {
-            ContextCompat.checkSelfPermission(
-                requireContext(),
-                locationPermission
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                return true
-            }
-
-            requireActivity().shouldShowRequestPermissionRationale(locationPermission) -> {
-                findNavController().navigate(R.id.nav_location_permission_explanation)
-            }
-
-            else -> {
-//                requestLocationPermissionLauncher?.launch(locationPermission)
-                findNavController().navigate(R.id.nav_location_permission_explanation)
-            }
-        }
-        return false
+    override fun onLocationPermissionIsNotGranted() {
+        binding.cityNameTextView.text = "Доступ к локации не предоставлен"
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        requestLocationPermissionLauncher = null
     }
 }
